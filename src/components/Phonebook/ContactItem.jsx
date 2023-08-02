769c93f9d13123c38a8f82b0c99d908f6d29a40a
1Oks3Ana2 <38954729+oksunlght@users.noreply.github.com>
2023-08-02 03:44:22 +0300

    Update ContactItem.jsx

--- src/components/Phonebook/ContactItem.jsx
+++ src/components/Phonebook/ContactItem.jsx
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { Item } from './Phonebook.styled';
-
-// const ContactItem = ({ id, name, number, children }) => {
-//   return (
-//     <Item>
-//       &#8226; {name} {number}
-//       {children}
-//     </Item>
-//   );
-// };
-
-// export default ContactItem;
-
-// ContactItem.propTypes = {
-//   id: PropTypes.string.isRequired,
-//   name: PropTypes.string.isRequired,
-//   number: PropTypes.string.isRequired,
-// };
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Item, DeleteBtn } from './Phonebook.styled';
